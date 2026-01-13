c2d2461d050e1cc374174c51d9c5821f78edafe3
Jocelin <jocelinreyesrodriguez2@gmail.com>
2026-01-13 02:29:26 -0600

    Agregando documentacion Swagger

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, request
-from flasgeger import Swagger
+from flasgger import Swagger
 from routes.habitos import habitos_bp
 from routes.categorias_habitos import categorias_bp
 from routes.seguimiento_habitos import seguimiento_bp
 from routes.estadisticas_habitos import estadisticas_bp
 # 1. IMPORTA EL BLUEPRINT DE ECONOMÍA
 from routes.economia import economia_bp 
+import os
 
 app = Flask(__name__)
 swagger = Swagger(app)
@@ -17,6 +18,8 @@ app.register_blueprint(seguimiento_bp)
 app.register_blueprint(estadisticas_bp)
 app.register_blueprint(economia_bp) # <-- ASEGÚRATE DE QUE ESTA LÍNEA ESTÉ AQUÍ
 
+
+
 if __name__ == "__main__":
-    # Asegúrate de usar host="0.0.0.0" para que tu celular pueda conectar
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
