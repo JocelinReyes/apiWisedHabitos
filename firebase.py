609d588f47612d50a79fb10f44dcd9465fbaf3f2
Jocelin <jocelinreyesrodriguez2@gmail.com>
2026-01-13 02:34:07 -0600

    Agregando documentacion Swagger

--- firebase.py
+++ firebase.py
@@ -1,9 +1,14 @@
-# firebase.py
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("serviceAccountKey.json")
+firebase_key = os.environ.get("FIREBASE_KEYS")
 
+if not firebase_key:
+    raise ValueError("FIREBASE_KEYS no está definida")
+
+cred = credentials.Certificate(json.loads(firebase_key))
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
